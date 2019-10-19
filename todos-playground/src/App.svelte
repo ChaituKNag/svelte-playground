06c3157d0eb698e1fad4aa709c716bb0bd24acbd
K.N.C <knc@outlook.in>
2019-10-19 11:38:56 +0530

    fetching list of users

---
+++ todos-playground/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import UsersList from './components/UsersList.svelte';
+	export let name;
+	let imgSrc = `https://picsum.photos/id/1041/536/354`;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<!-- <h1>Hello {name}!</h1> -->
+<!-- <img src={imgSrc} alt="My Image"> -->
+<UsersList />
---
+++ todos-playground/src/components/UsersList.svelte
@@ -0,0 +1,27 @@
+<script>
+    let users = fetchUsers();
+    
+    async function fetchUsers() {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        const u = await res.json();
+
+        if(u) {
+            console.log(u);
+            return u;
+        } else {
+            throw new Error();
+        }
+    }
+</script>
+
+{#await users}
+<p>Loading...</p>
+{:then usersList}
+<ul>
+    {#each usersList as user}
+        <li>{user.name}</li>
+    {/each}
+</ul>
+{:catch}
+<p>An error occurred!</p>
+{/await}
