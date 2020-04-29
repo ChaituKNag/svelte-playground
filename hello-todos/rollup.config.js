a6558eb501b19e3b1e013ad910190b44dff913d3
nkonada <knc@outlook.in>
2020-04-29 19:56:31 +0530

    custom elements working fine.

--- hello-todos/rollup.config.js
+++ hello-todos/rollup.config.js
@@ -16,14 +16,11 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
       css: css => {
         css.write("public/build/bundle.css");
       },
-      customElement: true
+      customElement: production
     }),
 
     // If you have external dependencies installed from
