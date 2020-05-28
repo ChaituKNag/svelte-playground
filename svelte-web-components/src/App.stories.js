424bc3d0128e3f65a78890aabd0fc4a4f44aa674
nkonada <knc@outlook.in>
2020-05-28 15:18:47 +0530

    svelte-web-components: initial commit

---
+++ svelte-web-components/src/App.stories.js
@@ -0,0 +1,13 @@
+import App from "./App.svelte";
+
+export default {
+  title: "App",
+  component: App
+};
+
+export const Default = () => ({
+  Component: App,
+  props: {
+    name: "Naga Chaitanya Konada"
+  }
+});
